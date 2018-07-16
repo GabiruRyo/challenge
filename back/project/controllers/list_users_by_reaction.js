c09333e87827c303e5ebe71ec645b2e394437191
Gabriel Ryo Hioki <gabiruryo@gmail.com>
2018-07-16 20:20:06 -0300

    Back modificado com alguns erros ainda.

--- back/project/controllers/list_users_by_reaction.js
+++ back/project/controllers/list_users_by_reaction.js
@@ -1,6 +1,30 @@
+/*  Funcao assincrona que procura reacoes especificas em textos
+ * e retorna o usuario que teve a reacao.
+ */
 module.exports = async function(base, by_text, by_reaction) {
     try{
         let users = []
+        let found_reactions = await base.reactions.find({
+            reaction  :  by_reaction,
+        })
+        .populate('post')
+        .populate('user')
+        console.log(found_reactions.length)
+
+        found_reactions = found_reactions.filter((reaction) => {
+            return (reaction.post.title.includes(by_text)) || (reaction.post.text.includes(by_text))
+        })
+        console.log(found_reactions.length)
+
+        for(let r = 0; r < found_reactions.length; r++) {
+            let found_user = found_reactions[r].user
+            if(found_user && users.indexOf(found_user.username) === -1) {
+                users.push(found_user.username)
+            }
+        }
+        console.log(users)
+
+        /*
         let found_posts = await base.posts.find({
             title: new RegExp(by_text, 'g'),
             text: new RegExp(by_text, 'g')
@@ -19,6 +43,7 @@ module.exports = async function(base, by_text, by_reaction) {
                 }
             }
         }
+        */
         return users.sort((user_a, user_b) => user_a.localeCompare(user_b))
     }catch(err){
         console.error(err)
