dc8c6b9754c457b2e3cd08c70cba0bd29d10868a
Gabriel Ryo Hioki <gabiruryo@gmail.com>
2018-07-18 11:19:03 -0300

    back-end feito

--- back/project/controllers/list_users_by_reaction.js
+++ back/project/controllers/list_users_by_reaction.js
@@ -4,46 +4,30 @@
 module.exports = async function(base, by_text, by_reaction) {
     try{
         let users = []
+
+        // Pega apenas os objetos reactions com a reacao 'by_reaction'.
         let found_reactions = await base.reactions.find({
             reaction  :  by_reaction,
         })
+
+        // Popula os atributos para poderem ser acessados diretamente no filtro.
         .populate('post')
         .populate('user')
-        console.log(found_reactions.length)
 
+        // Filtra apenas os posts com a string 'by_text' no texto e no titulo.
         found_reactions = found_reactions.filter((reaction) => {
-            return (reaction.post.title.includes(by_text)) || (reaction.post.text.includes(by_text))
+            return (reaction.post.title.includes(by_text)) && (reaction.post.text.includes(by_text))
         })
-        console.log(found_reactions.length)
 
+        // Pega os usuarios que tiveram a reacao 'by_reaction' nos posts filtrados
         for(let r = 0; r < found_reactions.length; r++) {
             let found_user = found_reactions[r].user
             if(found_user && users.indexOf(found_user.username) === -1) {
                 users.push(found_user.username)
             }
         }
-        console.log(users)
 
-        /*
-        let found_posts = await base.posts.find({
-            title: new RegExp(by_text, 'g'),
-            text: new RegExp(by_text, 'g')
-        })
-        for(let p = 0; p < found_posts.length; p++){
-            let found_post = found_posts[p]
-            let found_reactions = await base.reactions.find({post: found_post._id})
-            found_reactions = found_reactions.filter((reaction) => {
-                return reaction.reaction == by_reaction
-            })
-            for(let r = 0; r < found_reactions.length; r++){
-                let found_reaction = found_reactions[r]
-                let found_user = await base.users.findById(found_reaction.user)
-                if(found_user && users.indexOf(found_user.username) === -1){
-                    users.push(found_user.username)
-                }
-            }
-        }
-        */
+        // Retorna um vetor de users ordenados.
         return users.sort((user_a, user_b) => user_a.localeCompare(user_b))
     }catch(err){
         console.error(err)
